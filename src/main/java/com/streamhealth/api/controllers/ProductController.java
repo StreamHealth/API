0079c4279e5feb8f853e99c129ed3bdc59f71a1f
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-12 21:59:31 +0800

    hehe products

--- src/main/java/com/streamhealth/api/controllers/ProductController.java
+++ src/main/java/com/streamhealth/api/controllers/ProductController.java
@@ -10,12 +10,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/product")
 public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/product/get_products")
+    @GetMapping("/get_products")
     public ResponseEntity<List<ProductDto>> getProducts(@RequestParam(required = false) String query) {
         List<ProductDto> productsData;
         if(query != null) {
@@ -26,26 +26,26 @@ public class ProductController {
         return ResponseEntity.ok(productsData);
     }
 
-    @GetMapping("/product/get_product/{productId}")
+    @GetMapping("/get_product/{productId}")
     public ResponseEntity<ProductDto> getProductById(@PathVariable Long productId) {
         ProductDto productData = productService.getProductById(productId);
         return ResponseEntity.ok(productData);
     }
 
-    @PostMapping("/product/add_product")
+    @PostMapping("/add_product")
     public ResponseEntity<?> addProduct(@RequestBody ProductDto productDto) {
         productService.validateProductDto(productDto);
         ProductDto productData = productService.addProduct(productDto);
         return ResponseEntity.ok(productData);
     }
-    @PutMapping("/product/update_product/{productId}")
+    @PutMapping("/update_product/{productId}")
     public ResponseEntity<?> updateProduct(@PathVariable Long productId, @RequestBody ProductDto productDto) {
         productService.validateProductDto(productDto);
         ProductDto productData = productService.updateProduct(productId, productDto);
         return ResponseEntity.ok(productData);
     }
 
-    @DeleteMapping("/product/delete_product/{productId}")
+    @DeleteMapping("/delete_product/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long productId) {
         productService.deleteProduct(productId);
         return ResponseEntity.ok("Product deleted successfully: " + productId);
